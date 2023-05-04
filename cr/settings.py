eb4ad899f1c6eeb0363cfd9abef5cb3b5a21e9b7
Tom Carrick <tom@carrick.eu>
2023-05-04 11:10:52 +0200

    Add EPC ingest and view

--- cr/settings.py
+++ cr/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "cr.apps.CrConfig",
+    "cr",
+    "rest_framework",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
